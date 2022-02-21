a97594497e3808a658365c63334ab1781cc75cad
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-02-21 10:17:38 +0900

    feat: Ctrl-] で ghq list を fzf で選択

--- .config/zsh/bindkey.zsh
+++ .config/zsh/bindkey.zsh
@@ -1,5 +1,16 @@
 # Emacs 風のキーバインドを使用 (他に Vim 風もある)
 bindkey -e 
 
+function ghq-fzf() {
+  local src="$(ghq list | fzf)"
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^]' ghq-fzf
+
 # Shift+Tab で前の選択肢へ移動
 bindkey '^[[Z' reverse-menu-complete
