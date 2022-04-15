9cc785abcf1dda67f99762c92ea0561d31072db2
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-04-16 00:39:54 +0900

    fix: cmp keymap

--- .config/nvim/lua/rc/plugin_config/nvim-cmp.lua
+++ .config/nvim/lua/rc/plugin_config/nvim-cmp.lua
@@ -11,6 +11,15 @@ cmp.event:on('confirm_done',
   cmp_autopairs.on_confirm_done({ map_char = { tex = '' }})
 )
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -21,6 +30,25 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-q>'] = cmp.mapping.close(),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
   },
   sources = {
     {name = 'nvim_lsp'},
