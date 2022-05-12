28fd86597066de96911b90aff05e0a31e2bb5bc4
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-05-13 00:30:25 +0900

    nvim: bufferline等の読み込み順序を修正してハイライトが正しく適用されるよう修正

--- .config/nvim/lua/rc/pluginconfig/after_launch.lua
+++ .config/nvim/lua/rc/pluginconfig/after_launch.lua
@@ -10,61 +10,6 @@ do
 	end
 end
 
--- https://github.com/akinsho/bufferline.nvim#configuration
-do -- Bufferline
-	require('bufferline').setup {
-		options = {
-			mode = "buffers", -- set to "tabs" to only show tabpages instead
-			numbers = "both",
-			close_command = "bdelete! %d",
-			right_mouse_command = nil,
-			left_mouse_command = "buffer %d",
-			middle_mouse_command = "bdelete! %d",
-			max_name_length = 18,
-			max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-			tab_size = 18,
-			diagnostics = "nvim_lsp",
-			diagnostics_update_in_insert = false,
-			diagnostics_indicator = function(count, _, _, _)
-				return "("..count..")"
-			end,
-			-- NOTE: this will be called a lot so don't do any heavy processing here
-			custom_filter = function(buf_number, _)
-				if vim.bo[buf_number].filetype == "qf" then
-					return false
-				end
-				return true
-			end,
-			color_icons = true,
-			show_buffer_icons = true,
-			show_buffer_close_icons = false,
-			show_buffer_default_icon = false, -- whether or not an unrecognised filetype should show a default icon
-			show_close_icon = false,
-			show_tab_indicators = true,
-			persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-			separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' }
-			enforce_regular_tabs = false,
-			always_show_bufferline = true,
-		}
-	}
-	keymap.set("n", "[b", "<Cmd>BufferLineCyclePrev<CR>", { noremap=true, silent=true })
-	keymap.set("n", "]b", "<Cmd>BufferLineCycleNext<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",bp", "<Cmd>BufferLinePick<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",bd", "<Cmd>lua require'bufdelete'.bufdelete(0, false)<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",bH", "<Cmd>BufferLineMovePrev<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",bL", "<Cmd>BufferLineMoveNext<CR>", { noremap=true, silent=true })
-
-	keymap.set("n", ",1", "<Cmd>BufferLineGoToBuffer 1<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",2", "<Cmd>BufferLineGoToBuffer 2<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",3", "<Cmd>BufferLineGoToBuffer 3<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",4", "<Cmd>BufferLineGoToBuffer 4<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",5", "<Cmd>BufferLineGoToBuffer 5<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",6", "<Cmd>BufferLineGoToBuffer 6<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",7", "<Cmd>BufferLineGoToBuffer 7<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",8", "<Cmd>BufferLineGoToBuffer 8<CR>", { noremap=true, silent=true })
-	keymap.set("n", ",9", "<Cmd>BufferLineGoToBuffer 9<CR>", { noremap=true, silent=true })
-end
-
 
 -- https://github.com/mattn/vim-sonictemplate/blob/master/doc/sonictemplate.txt
 do -- sonictemplate
@@ -268,9 +213,6 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = setup_highlight
 })
 
-vim.cmd "colorscheme duskfox"
-setup_highlight()
-
 require('indent_blankline').setup {
 	char = "",
 	char_highlight_list = {
@@ -283,8 +225,9 @@ require('indent_blankline').setup {
 	},
 	show_trailing_blankline_indent = false,
 }
-require'colorizer'.setup()
-require'todo-comments'.setup()
+
+vim.cmd "colorscheme duskfox"
+setup_highlight()
 
 -- https://github.com/nvim-lualine/lualine.nvim
 do  -- Statusline
@@ -347,3 +290,61 @@ do  -- Statusline
 		},
 	}
 end
+
+-- https://github.com/akinsho/bufferline.nvim#configuration
+do -- Bufferline
+	require('bufferline').setup {
+		options = {
+			mode = "buffers", -- set to "tabs" to only show tabpages instead
+			numbers = "both",
+			close_command = "bdelete! %d",
+			right_mouse_command = nil,
+			left_mouse_command = "buffer %d",
+			middle_mouse_command = "bdelete! %d",
+			max_name_length = 18,
+			max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+			tab_size = 18,
+			diagnostics = "nvim_lsp",
+			diagnostics_update_in_insert = false,
+			diagnostics_indicator = function(count, _, _, _)
+				return "("..count..")"
+			end,
+			-- NOTE: this will be called a lot so don't do any heavy processing here
+			custom_filter = function(buf_number, _)
+				if vim.bo[buf_number].filetype == "qf" then
+					return false
+				end
+				return true
+			end,
+			color_icons = true,
+			show_buffer_icons = true,
+			show_buffer_close_icons = false,
+			show_buffer_default_icon = false, -- whether or not an unrecognised filetype should show a default icon
+			show_close_icon = false,
+			show_tab_indicators = true,
+			persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+			separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' }
+			enforce_regular_tabs = false,
+			always_show_bufferline = true,
+		}
+	}
+	keymap.set("n", "[b", "<Cmd>BufferLineCyclePrev<CR>", { noremap=true, silent=true })
+	keymap.set("n", "]b", "<Cmd>BufferLineCycleNext<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",bp", "<Cmd>BufferLinePick<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",bd", "<Cmd>lua require'bufdelete'.bufdelete(0, false)<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",bH", "<Cmd>BufferLineMovePrev<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",bL", "<Cmd>BufferLineMoveNext<CR>", { noremap=true, silent=true })
+
+	keymap.set("n", ",1", "<Cmd>BufferLineGoToBuffer 1<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",2", "<Cmd>BufferLineGoToBuffer 2<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",3", "<Cmd>BufferLineGoToBuffer 3<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",4", "<Cmd>BufferLineGoToBuffer 4<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",5", "<Cmd>BufferLineGoToBuffer 5<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",6", "<Cmd>BufferLineGoToBuffer 6<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",7", "<Cmd>BufferLineGoToBuffer 7<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",8", "<Cmd>BufferLineGoToBuffer 8<CR>", { noremap=true, silent=true })
+	keymap.set("n", ",9", "<Cmd>BufferLineGoToBuffer 9<CR>", { noremap=true, silent=true })
+end
+
+require'colorizer'.setup()
+require'todo-comments'.setup()
