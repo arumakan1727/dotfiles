1cedb637046b4c14418fd994ee137a3307b59ffb
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-13 19:51:37 +0900

    add(nvim): tree-sitter

---
+++ homedir/.config/nvim/lua/armkn/plugins/treesitter.lua
@@ -0,0 +1,64 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			{
+				"andymass/vim-matchup",
+				init = function()
+					vim.g.matchup_matchparen_offscreen = { method = "popup" }
+				end,
+			},
+		},
+		keys = {
+			{ "<c-space>", desc = "Increment selection" },
+			{ "<bs>", desc = "Decrement selection", mode = "x" },
+		},
+		---@type TSConfig
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			matchup = { enable = true },
+			context_commentstring = { enable = true, enable_autocmd = false },
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"go",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"rust",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = "<nop>",
+					node_decremental = "<bs>",
+				},
+			},
+		},
+		---@param opts TSConfig
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+}
