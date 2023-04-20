73e4ad83b8d471dd876b96ece5839bb8251979a3
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-20 12:49:59 +0900

    add(nvim): plugin dressing.nvim

--- homedir/.config/nvim/lua/armkn/plugins/ui.lua
+++ homedir/.config/nvim/lua/armkn/plugins/ui.lua
@@ -1,5 +1,21 @@
 return {
 	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = { "BufReadPost", "BufNewFile" },
 		opts = function()
