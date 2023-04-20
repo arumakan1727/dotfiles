723232d5bcd8bfc5a7ec83e14bd4c1d5cbd90954
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-20 16:46:38 +0900

    nvim: add plugin nvim_lsp_signature_help

--- homedir/.config/nvim/lua/armkn/plugins/completion.lua
+++ homedir/.config/nvim/lua/armkn/plugins/completion.lua
@@ -14,6 +14,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"saadparwaiz1/cmp_luasnip",
 			"onsails/lspkind.nvim",
 		},
@@ -44,6 +45,7 @@ return {
 					}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
+					{ name = "nvim_lsp_signature_help" },
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "buffer" },
