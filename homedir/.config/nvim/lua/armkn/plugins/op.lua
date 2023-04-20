216d5ef5ad37d22526fdad8ef1dc4078e3af789d
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-20 21:06:57 +0900

    nvim: add custom text obj `s` (alpha numeric, except underbar)

--- homedir/.config/nvim/lua/armkn/plugins/op.lua
+++ homedir/.config/nvim/lua/armkn/plugins/op.lua
@@ -25,6 +25,11 @@ return {
 	{
 		"echasnovski/mini.ai",
 		event = "ModeChanged",
+		opts = {
+			custom_textobjects = {
+				["s"] = { "%f[%w]%w+", "^().*()$" }, -- like `w`, except underbar
+			},
+		},
 		config = function(_, opts)
 			require("mini.ai").setup(opts)
 		end,
