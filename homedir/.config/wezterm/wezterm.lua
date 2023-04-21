4365f83e98304f33194bc7445b7df37bdc1e8e67
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-21 14:37:23 +0900

    style: fmt

--- homedir/.config/wezterm/wezterm.lua
+++ homedir/.config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Tokyo Night (Gogh)'
+config.color_scheme = "Tokyo Night (Gogh)"
 config.colors = {
-	cursor_bg = '#f0f0f0',
-	cursor_fg = '#000000',
-	cursor_border = '#ff0000',
+	cursor_bg = "#f0f0f0",
+	cursor_fg = "#000000",
+	cursor_border = "#ff0000",
 }
 
 config.send_composed_key_when_left_alt_is_pressed = false
@@ -17,18 +17,18 @@ config.send_composed_key_when_right_alt_is_pressed = true
 config.keys = {
 	{
 		-- On mac with JIS keyboard
-		key = '¥',
-		mods = 'CTRL',
-		action = wezterm.action.SendKey {
-			key = '\\',
-			mods = 'CTRL',
-		},
+		key = "¥",
+		mods = "CTRL",
+		action = wezterm.action.SendKey({
+			key = "\\",
+			mods = "CTRL",
+		}),
 	},
 	{
-		key = 'r',
-		mods = 'CTRL|SHIFT',
+		key = "r",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.ReloadConfiguration,
-	}
+	},
 }
 
 return config
