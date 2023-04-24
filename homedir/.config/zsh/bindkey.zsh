de2aac3034b7bbbbed3ab3c0f61ba6084db0a75b
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-04-24 20:34:21 +0900

    feat(zsh): bindkey Ctrl+] to ghq-fzf-tmux

--- homedir/.config/zsh/bindkey.zsh
+++ homedir/.config/zsh/bindkey.zsh
@@ -1,16 +1,41 @@
 # Emacs 風のキーバインドを使用 (他に Vim 風もある)
 bindkey -e
 
-function ghq-fzf() {
-  local src="$(ghq list | fzf)"
-  if [ -n "$src" ]; then
-    BUFFER="cd $(ghq root)/$src"
-    zle accept-line
+## ghq 配下のリポジトリを fzf で選択 -> 自動で tmux セッションの作成/リネーム
+function ghq-fzf-tmux() {
+  local repo dir target_session current_session
+
+  repo="$(ghq list | fzf)"
+  [[ -z "$repo" ]] && return 1
+
+  dir="$(ghq root)/$repo"
+
+  if [[ -z $TMUX ]]; then
+    BUFFER="cd $dir"
+    zle accept-line && zle -R -c
+    return 0
   fi
-  zle -R -c
+
+  # strip first path component
+  # ex) "github.com/arumakan1727/dotfiles" -> "arumakan1727/dotfiles"
+  target_session="${repo#*/}"
+  current_session="$(tmux display-message -pF '#{session_name}')"
+
+  if [[ "$current_session" = "$target_session" ]]; then
+    BUFFER="cd $dir"
+    zle accept-line && zle -R -c
+    return 0
+  fi
+
+  # もし target_session が存在しなければセッションを作成
+  if [[ -z "$(tmux ls -F '#S' -f "#{m:${target_session},#S}")" ]]; then
+    tmux new-session -d -c "$dir" -s "$target_session"
+  fi
+  tmux switch-client -t "$target_session"
 }
-zle -N ghq-fzf
-bindkey '^]' ghq-fzf
+
+zle -N ghq-fzf-tmux
+bindkey '^]' ghq-fzf-tmux
 
 # Shift+Tab で前の選択肢へ移動
 bindkey '^[[Z' reverse-menu-complete
