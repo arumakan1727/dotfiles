b64fe9ce07abb340179a9e9f011b5c6514bb9e46
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-05-05 12:20:42 +0900

    nvim: support filetype `*.envrc`, `*.sample`

--- homedir/.config/nvim/filetype.lua
+++ homedir/.config/nvim/filetype.lua
@@ -2,8 +2,16 @@ vim.filetype.add({
 	extension = {
 		bnf = "ebnf",
 		ebnf = "ebnf",
+		envrc = "sh",
+
+		---@param path string
+		---@param bufnr integer
+		sample = function(path, bufnr)
+			return vim.filetype.match({ buf = bufnr, filename = path:gsub("%.sample$", "") })
+		end,
 	},
 	filename = {
+		[".envrc"] = "sh",
 		[".flake8"] = "cfg",
 	},
 })
