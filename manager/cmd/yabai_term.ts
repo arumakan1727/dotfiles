7d3143d3c77339b7a1d4c2e977f1cb4417586deb
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-05-13 16:16:43 +0900

    yabai: ターミナルを隠した後に現在のスペースにウィンドウが表示されていない場合はrecentウィンドウにフォーカスしない (現在のスペースにとどまる)

--- manager/cmd/yabai_term.ts
+++ manager/cmd/yabai_term.ts
@@ -6,72 +6,128 @@
  *    is-sticky=true  ->  全てのワークスペースに表示
  * これにより、ドロップダウンターミナルのような挙動を再現できる。
  */
-import { streams } from "../deps.ts";
 
-const windowInfoMock = {
-  id: 1,
-  "is-visible": false,
-  "is-floating": false,
-  "is-sticky": false,
-  "is-topmost": false,
+type WindowID = number;
+
+/** `yabai -m query windows` で得られる要素のスキーマ */
+type WindowInfo = {
+  id: WindowID;
+  pid: number;
+  app: string;
+  title: string;
+  display: number;
+  space: number;
+  "can-move": boolean;
+  "can-resize": boolean;
+  "has-focus": boolean;
+  "has-shadow": boolean;
+  "has-border": boolean;
+  "has-parent-zoom": boolean;
+  "has-fullscreen-zoom": boolean;
+  "is-native-fullscreen": boolean;
+  "is-visible": boolean;
+  "is-minimized": boolean;
+  "is-hidden": boolean;
+  "is-floating": boolean;
+  "is-sticky": boolean;
+  "is-topmost": boolean;
+  "is-grabbed": boolean;
 };
 
-const windowInfoProps: readonly string[] = Object.keys(windowInfoMock);
-type WindowInfo = typeof windowInfoMock;
+/** `yabai -m spaces` で得られる要素のスキーマ */
+type SpaceInfo = {
+  id: number;
+  uuid: string;
+  index: number;
+  label: string;
+  type: string;
+  display: number;
+  windows: WindowID[];
+  "first-window": WindowID;
+  "last-window": WindowID;
+  "has-focus": boolean;
+  "is-visible": boolean;
+  "is-native-fullscreen": boolean;
+};
 
-async function fetchMinimumIDWinInfo(targetAppName: string): Promise<WindowInfo> {
-  const yabai = new Deno.Command("yabai", {
-    args: ["-m", "query", "--windows"],
-    stdin: "inherit",
-    stdout: "piped",
-    stderr: "inherit",
-  }).spawn();
-
-  const jq = new Deno.Command("jq", {
-    args: [
-      "-r",
-      [
-        `[.[] | select(.app | contains("${targetAppName}"))]`,
-        "sort_by(.id)",
-        ".[0]",
-        `{ ${windowInfoProps.map((s) => `"${s}"`).join(", ")} }`,
-      ].join(" | "),
-    ],
-    stdin: "piped",
-    stdout: "piped",
-    stderr: "inherit",
-  }).spawn();
-
-  yabai.stdout.pipeTo(jq.stdin);
-  {
-    const { success, code } = await yabai.status;
-    if (!success) throw new Error(`failed to run: \`yabai\` returned ${code}`);
+async function queryYabaiWindowInfos(spaceIndex?: number): Promise<WindowInfo[]> {
+  const args = ["-m", "query", "--windows"];
+  if (spaceIndex !== undefined) {
+    args.push("--space", spaceIndex.toString());
   }
-  {
-    const { success, code, stdout } = await jq.output();
-    if (!success) throw new Error(`failed to run: \`jq\` returned ${code}`);
-    const res = new TextDecoder().decode(stdout);
-    return JSON.parse(res);
+
+  const yabai = new Deno.Command("yabai", { args, stderr: "inherit" });
+  const { success, code, stdout } = await yabai.output();
+  if (!success) {
+    throw new Error(`failed to run: exitcode=${code}: yabai ${args.join(" ")}`);
   }
+
+  const winInfos = JSON.parse(new TextDecoder().decode(stdout));
+  return winInfos;
 }
 
-function generateYabaiTermCmd(w: WindowInfo): string[] {
-  const id = w.id.toString();
+async function queryYabaiSpaceInfos(): Promise<SpaceInfo[]> {
+  const args = ["-m", "query", "--spaces"];
+  const yabai = new Deno.Command("yabai", { args, stderr: "inherit" });
+  const { success, code, stdout } = await yabai.output();
+  if (!success) {
+    throw new Error(`failed to run: exitcode=${code}: yabai ${args.join(" ")}`);
+  }
+
+  return JSON.parse(new TextDecoder().decode(stdout));
+}
+
+async function fetchMinimumIDWinInfo(targetAppName: string): Promise<WindowInfo> {
+  const winInfos = await queryYabaiWindowInfos();
+  const ws = winInfos
+    .filter((w) => w.app === targetAppName)
+    .sort((a, b) => a.id - b.id);
+  return ws[0];
+}
+
+async function fetchCurrentSpaceIndex(): Promise<number> {
+  const spaceInfos = await queryYabaiSpaceInfos();
+  const focusedSpace = spaceInfos.filter((s) => s["has-focus"])[0];
+  return focusedSpace.index;
+}
+
+async function fetchFirstVisibleWin(opt: {
+  spaceIndex: number;
+  ignoreAppName: string;
+}): Promise<WindowInfo | undefined> {
+  const { spaceIndex, ignoreAppName } = opt;
+  const winInfos = await queryYabaiWindowInfos(spaceIndex);
+  const ws = winInfos.filter((w) => w["is-visible"] && w.app !== ignoreAppName);
+  return ws.length > 0 ? ws[0] : undefined;
+}
+
+async function generateYabaiTermCmd(termWin: WindowInfo): Promise<string[]> {
+  const id = termWin.id.toString();
   const cmd: string[] = ["yabai", "-m", "window", id];
-  if (!w["is-floating"]) cmd.push("--toggle", "float");
-  if (!w["is-sticky"]) cmd.push("--toggle", "sticky");
-  if (!w["is-topmost"]) cmd.push("--toggle", "topmost");
-  if (w["is-visible"]) {
-    cmd.push("--minimize", id, "--focus", "recent");
-  } else {
+  if (!termWin["is-floating"]) cmd.push("--toggle", "float");
+  if (!termWin["is-sticky"]) cmd.push("--toggle", "sticky");
+  if (!termWin["is-topmost"]) cmd.push("--toggle", "topmost");
+
+  if (!termWin["is-visible"]) {
     cmd.push("--deminimize", id, "--focus", id);
+  } else {
+    cmd.push("--minimize", id);
+
+    const currentSpace = await fetchCurrentSpaceIndex();
+    const visibleWindowInSameSpace = await fetchFirstVisibleWin({
+      spaceIndex: currentSpace,
+      ignoreAppName: termWin.app,
+    });
+    if (visibleWindowInSameSpace != null) {
+      cmd.push("--minimize", id, "--focus", visibleWindowInSameSpace.id.toString());
+    }
   }
   return cmd;
 }
 
 async function main() {
   const w = await fetchMinimumIDWinInfo("WezTerm");
-  const cmd = generateYabaiTermCmd(w);
+  const cmd = await generateYabaiTermCmd(w);
   console.log(cmd.join(" "));
 
   const p = new Deno.Command(cmd[0], { args: cmd.slice(1), stderr: "inherit" });
