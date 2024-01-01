ab89160aa3005daa32ee089a09eb809a0a586390
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-01-01 22:31:19 +0900

    chore: add installer/rye.sh

---
+++ installer/rye.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+export RYE_HOME="${RYE_HOME:-$HOME/.rye}"
+
+if [[ ! -x "$RYE_HOME/shims/rye" ]]; then
+  curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
+fi
+
+"$RYE_HOME/shims/rye" config --set-bool behavior.global-python=true
