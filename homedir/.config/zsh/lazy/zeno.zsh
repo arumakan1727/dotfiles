e972d8decd6ee92e44a70331bce2238d4cbdf331
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-01-13 15:54:16 +0900

    zsh: add fuction `kill_all_zeno`

--- homedir/.config/zsh/lazy/zeno.zsh
+++ homedir/.config/zsh/lazy/zeno.zsh
@@ -14,3 +14,8 @@ if [[ -n $ZENO_LOADED ]]; then
   bindkey '^x^s' zeno-insert-snippet
   bindkey '^r'   zeno-history-selection
 fi
+
+# kill all zeno process to spawn new zeno with loading latest config.toml
+function kill_all_zeno {
+  ps -o 'pid=,command=' | sed -nE 's/^([0-9]+)\s+deno run .*zeno\.zsh.*/\1/p' | xargs kill
+}
