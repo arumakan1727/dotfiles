24d2ce1f88780e471b32f370c98885a41e486c83
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-10-21 18:03:23 +0900

    chore: install cargo-binsintall in installer/rustup.sh

--- installer/rustup.sh
+++ installer/rustup.sh
@@ -6,8 +6,11 @@ export CARGO_HOME="${CARGO_HOME:-$HOME/.cargo}"
 
 if [[ -d "$RUSTUP_HOME/toolchains" && -x "$CARGO_HOME/bin/rustup" ]]; then
   echo "[OK] rustup is already installed!"
-  exit
+else
+  # install stable toolchain
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
+    sh -s -- -y --no-modify-path --default-toolchain stable --profile default
 fi
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
-  sh -s -- -y --no-modify-path --default-toolchain stable --profile default
+# install cargo-binstall
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
