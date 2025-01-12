92963539efdd3388e7cfffefa0e734ce11390cea
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2025-01-12 17:32:44 +0900

    rayast: オーディオ出力先を切り替えるスクリプトコマンドを追加

---
+++ etc/raycast/commands/switch-audio-output-BuiltInSpeakerDevice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title SwitchAudioOutput-BuiltInSpeaker
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Arumakan1727
+
+SwitchAudioSource -t output -u BuiltInSpeakerDevice
